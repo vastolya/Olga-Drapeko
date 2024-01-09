0e563a1352dae07363c03edb4f32f12f2f7db665
vastolya <paramee@bk.ru>
2024-01-09 20:06:41 +0700

    quick fix

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,10 +7,15 @@ const About = () => {
     <div className="md:mx-[12.5vw] my-[11.11vh] grid grid-cols-6 md:gap-x-[1.25vw] text-[#1B1743]">
       <div className="col-span-2">123</div>
       <div className="col-span-3">
-        <h1 className="md:text-[3.33vh] md:leading-[3.51vh] font-bold">Обо мне</h1>
-       <div className={`${montserrat.className}`}>
-        <p>123</p>
-       </div>
+        <h1 className="md:text-[3.33vh] md:leading-[3.51vh] font-bold">
+          Обо мне
+        </h1>
+        <p>
+          Здравствуйте! Меня зовут <span>Ольга Драпеко</span>
+        </p>
+        <div className={`${montserrat.className}`}>
+          <p>123</p>
+        </div>
       </div>
     </div>
   );
